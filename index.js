bc9cbf260bfd3d72abbbd9ced93fc92344ae6501
Sydney J <sjennings16@my.bcit.ca>
2025-05-03 01:50:03 -0700

    Create a README.txt file for self-graded checklist.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
  * 4. node index.js -> to run the server
  * 5. npm install nodemon -g 
  * 6. nodemon index.js -> to run the server with nodemon
+ * 7. npm install dotenv
+ * 8. npm install joi
+ * 
  */
 
 
