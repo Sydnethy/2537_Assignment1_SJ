6ee275139e0bd1028bab4f1d158557a6d2d1f4a8
Sydney J <sjennings16@my.bcit.ca>
2025-05-03 01:28:43 -0700

    Created index.js with node.js and express with node modules.

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Create github repo project.
+ * Set up application by:
+ * 1. npm init
+ * 2. npm install express
+ * 3. Add *express root of project code* to index.js file
+ * 4. node index.js -> to run the server
+ * 5. npm install nodemon -g 
+ * 6. nodemon index.js -> to run the server with nodemon
+ */
+
+
+//Express root of project code 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
